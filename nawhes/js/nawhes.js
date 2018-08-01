365eea150179b5d0052591e07098cf082c849e39
Sehwan <nawhes@naver.com>
2018-08-02 08:21:44 +0900

    180801

--- nawhes/js/nawhes.js
+++ nawhes/js/nawhes.js
@@ -4,7 +4,19 @@ $(function(){
 		// var index = $(this).css("grid-area","auto / auto / span 2 / span 2");
 		// console.log(index);
 
-		$(this).next().find(".comment").append("<input>").find("input").attr("type","password");
+		// $(this).next().find(".comment").prepend("<input>").find("input").attr("placeholder","What do you think?");
 		// $(this).next().find(".comment").find("input").attr("type","password");
+	});
+
+	$(".feed").css("grid-row", function(){
+		var feedgrid = "auto / ";
+		console.log(feedgrid);
+		var feedheight = $(this).find("img").height();
+		console.log(feedheight);
+		if (feedheight >= 200) {
+			 feedgrid += "span 2";
+		}
+
+		return feedgrid;
 	});
 });
