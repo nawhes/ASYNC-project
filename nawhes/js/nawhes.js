eb21ec1d541e006b0c00bda4d266dd629e8cda2d
Sehwan <nawhes@naver.com>
2018-08-02 04:18:23 +0900

    180801

--- nawhes/js/nawhes.js
+++ nawhes/js/nawhes.js
@@ -1,8 +1,10 @@
 $(function(){
-	$(".feed").click(function(){
-		$(this).toggleClass("showArticle");
+	$(".temp_A").click(function(){
+		$(this).parent().toggleClass("showArticle");
 		// var index = $(this).css("grid-area","auto / auto / span 2 / span 2");
 		// console.log(index);
 
+		$(this).next().find(".comment").append("<input>").find("input").attr("type","password");
+		// $(this).next().find(".comment").find("input").attr("type","password");
 	});
 });
