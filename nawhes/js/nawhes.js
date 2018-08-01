ff654ffd7d62bf5e7f45de69264de507b0688823
Sehwan <nawhes@naver.com>
2018-08-02 02:45:54 +0900

    180801

---
+++ nawhes/js/nawhes.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".feed").click(function(){
+		$(this).toggleClass("showArticle");
+		// var index = $(this).css("grid-area","auto / auto / span 2 / span 2");
+		// console.log(index);
+
+	});
+});
